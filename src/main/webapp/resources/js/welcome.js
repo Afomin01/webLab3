a45a6af5fffe3fb0bf9abd043da9e03fc7f8c5f0
Afomin <284718@niuitmo.ru>
2020-10-07 02:33:02 +0300

    Completed main page base appearance and logic

--- src/main/webapp/resources/js/welcome.js
+++ src/main/webapp/resources/js/welcome.js
@@ -1,8 +1,11 @@
 $(function() {
+    let t = getServerTime();
+
     setClockHands('client-date', new Date);
-    setClockHands('server-date', new Date( Date.now() - 1000 * 60 * 4));
+    setClockHands('server-date', new Date(t));
     setInterval(() => setClockHands('client-date', new Date), 5000);
-    setInterval(() => setClockHands('server-date', new Date( Date.now() - 1000 * 60 * 4)), 5000);
+    setInterval(() => setClockHands('server-date', new Date(getServerTime())), 5000);
+
 });
 
 function setClockHands(id, date) {
@@ -13,4 +16,28 @@ function setClockHands(id, date) {
     $(`#${id} .hourHand`).attr('transform', `rotate(${hourAngle})`);
     $(`#${id} .minuteHand`).attr('transform', `rotate(${minuteAngle})`);
     $(`#${id} .secondHand`).attr('transform', `rotate(${secondAngle})`);
+
+    let hours = date.getHours() > 9 ? date.getHours().toString() : "0"+ date.getHours().toString()
+    let minutes = date.getMinutes() > 9 ? date.getMinutes().toString() : "0"+ date.getMinutes().toString();
+    let seconds = date.getSeconds() > 9 ? date.getSeconds().toString() : "0"+ date.getSeconds().toString();
+    let day = date.getDate() > 9 ? date.getDate().toString() : "0"+ date.getDate().toString();
+    let month = (date.getMonth()+1) > 9 ? (date.getMonth()+1).toString() : "0"+ (date.getMonth()+1).toString();
+    let year = date.getFullYear();
+
+    let timeText = hours +":"+ minutes +":"+ seconds;
+    let dateText = day+"."+month+"."+year;
+
+    $(`#${id} .text-time`).text(timeText);
+    $(`#${id} .text-date`).text(dateText);
+}
+function getServerTime() {
+    let t;
+    $.ajax({
+        async: false,
+        type:"GET",
+        success: function(output, status, xhr) {
+            t = xhr.getResponseHeader("Date");
+        },
+    })
+    return t;
 }
