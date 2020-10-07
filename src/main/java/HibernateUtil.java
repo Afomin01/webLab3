5900d862742021bb03ae9c96f7eb30ab5f3d6e77
Dmitry Kupershtein <dmitry.kuper0@gmail.com>
2020-10-07 19:26:23 +0300

    create hiber template

---
+++ src/main/java/HibernateUtil.java
@@ -0,0 +1,22 @@
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.EntityManagerFactory;
+
+public class HibernateUtil {
+
+    private static final String PERSISTENT_UNIT_NAME = "orbis";
+
+    private static final EntityManagerFactory emf;
+
+    static {
+        try {
+            emf = Persistence.createEntityManagerFactory(PERSISTENT_UNIT_NAME);
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static EntityManager getEm() {
+        return emf.createEntityManager();
+    }
+}
