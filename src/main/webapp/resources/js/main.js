236d2565007b0a2e638b67aab12ef44001971249
Afomin <284718@niuitmo.ru>
2020-10-08 16:11:51 +0300

    Made coordinates send when graph clicked

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,7 +1,17 @@
-let graph = $('#graph');
+const fixedDigits = 7;
 
 $(function() {
-    graph.on('mousedown', (event) => {
+    $('#graph').on('mousedown', (event) => {
+        console.log("sdfsdgf")
+        let svg = document.getElementById('graph');
+        let pt = svg.createSVGPoint();
+        pt.x = event.clientX;
+        pt.y = event.clientY;
+        pt = pt.matrixTransform(svg.getScreenCTM().inverse());
+        let x = (pt.x - 175) / 35;
+        let y = (pt.y - 175) / -35;
 
+        $("#graph-x").val(x.toFixed(fixedDigits));
+        $("#graph-y").val(y.toFixed(fixedDigits));
     });
 });
