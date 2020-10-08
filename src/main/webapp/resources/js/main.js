ae73ab9e9954d547acfd362b85a9a1edc45d4242
Afomin <284718@niuitmo.ru>
2020-10-08 16:56:36 +0300

    Fixed coordinates calculation in main.js

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -2,14 +2,15 @@ const fixedDigits = 7;
 
 $(function() {
     $('#graph').on('mousedown', (event) => {
-        console.log("sdfsdgf")
+        let r = parseFloat($("#RSelect option:selected").text());
+        console.log(r);
         let svg = document.getElementById('graph');
         let pt = svg.createSVGPoint();
         pt.x = event.clientX;
         pt.y = event.clientY;
         pt = pt.matrixTransform(svg.getScreenCTM().inverse());
-        let x = (pt.x - 175) / 35;
-        let y = (pt.y - 175) / -35;
+        let x = (140/r)*pt.x+175;
+        let y = (140/r)*(-pt.y)+175;
 
         $("#graph-x").val(x.toFixed(fixedDigits));
         $("#graph-y").val(y.toFixed(fixedDigits));
