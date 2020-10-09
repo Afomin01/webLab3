e5f497baabf4e86686f0797a5affe7c3e070bafe
Afomin <284718@niuitmo.ru>
2020-10-09 21:07:10 +0300

    Added clear button and logic

--- src/main/java/EntryBean.java
+++ src/main/java/EntryBean.java
@@ -46,6 +46,11 @@ public class EntryBean {
         entries.add(newEntry);
     }
 
+    public void clearClientEntries(){
+        entries.clear();
+        EntryDao.deleteAllClientRows(clientId);
+    }
+
     public boolean check(double x, double y, double r) {
         if (x > 0 && y > 0) return false;
         else if (x < 0 && y > 0) return y < 2 * x + r;
--- src/main/java/EntryDao.java
+++ src/main/java/EntryDao.java
@@ -2,6 +2,7 @@ import Utils.HibernateUtil;
 
 import javax.persistence.EntityManager;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import java.util.List;
@@ -16,6 +17,14 @@ public class EntryDao {
         Root<Entry> c = cq.from(Entry.class);
         cq.where(cb.equal(c.get("clientId"), clientId));
         return em.createQuery(cq).getResultList();
+    }
+
+    public static void deleteAllClientRows(long clintId){
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaDelete<Entry> criteriaDelete = criteriaBuilder.createCriteriaDelete(Entry.class);
+        Root<Entry> root = criteriaDelete.from(Entry.class);
+        criteriaDelete.where(criteriaBuilder.equal(root.get("clientId"), clintId));
+        em.createQuery(criteriaDelete).executeUpdate();
     }
 
     public static void addEntry(Entry entry) {
